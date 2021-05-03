115c44d2257f4e89e7ad184cc6c0354e816f33a5
Matthew Thompson <supra__charged@hotmail.com>
2021-05-03 04:39:32 +0000

    Implement Task 3 in bottles.js and update its tests

--- bottles.js
+++ bottles.js
@@ -1,7 +1,13 @@
 const bottles = investment => {
-  if (typeof investment !== 'number' || investment < 0) {
-    return 0;
-  }
+  // Object to be returned - intitial state.
+  const bottleBreakDown = {
+    totalBottles: 0,
+    purchased: 0,
+    recycling: {
+      bottles: 0,
+      caps: 0
+    }
+  };
 
   // Recursive function that determines how many bottles can be redeemed based
   // on how many bottles and caps are passed in {bottles: Number, caps: Number}
@@ -13,7 +19,9 @@ const bottles = investment => {
 
     // Calculate the number of bottles can be redeemed from the input argument
     const fromBottles = Math.floor(bottleCapObj.bottles / 2);
+    bottleBreakDown.recycling.bottles += fromBottles;
     const fromCaps = Math.floor(bottleCapObj.caps / 4);
+    bottleBreakDown.recycling.caps += fromCaps;
     const redeemedSoFar = fromBottles + fromCaps;
 
     // Create a new object to represent bottles and caps available after redeeming
@@ -31,10 +39,15 @@ const bottles = investment => {
   // Determine initial amount of bottles (and caps) can be purchased with the
   // initial investment.
   const numBottles = Math.floor(investment / 2);
+  bottleBreakDown.purchased = numBottles;
 
+  if (typeof investment !== 'number' || investment < 0) {
+    return bottleBreakDown;
+  }
   // Initial investment + how many new bottles can be purchased from redeeming
   // caps and bottles will give the total.
-  return numBottles + calculateRedeemed({ bottles: numBottles, caps: numBottles });
+  bottleBreakDown.totalBottles = numBottles + calculateRedeemed({ bottles: numBottles, caps: numBottles });
+  return bottleBreakDown;
 };
 
 module.exports = bottles;
--- test/bottlesTest.js
+++ test/bottlesTest.js
@@ -3,34 +3,34 @@ const bottles = require('../bottles');
 
 describe('#bottles()', () => {
   it('should return 15 with an investment of 10', () => {
-    assert.strictEqual(bottles(10), 15);
+    assert.strictEqual(bottles(10).totalBottles, 15);
   });
 
   it('should return 35 with an investment of 20', () => {
-    assert.strictEqual(bottles(20), 35);
+    assert.strictEqual(bottles(20).totalBottles, 35);
   });
 
   it('should return 55 with an investment of 30', () => {
-    assert.strictEqual(bottles(30), 55);
+    assert.strictEqual(bottles(30).totalBottles, 55);
   });
 
   it('should return 75 with an investment of 40', () => {
-    assert.strictEqual(bottles(40), 75);
+    assert.strictEqual(bottles(40).totalBottles, 75);
   });
 
   it('should return 3 with an investment of 4', () => {
-    assert.strictEqual(bottles(4), 3);
+    assert.strictEqual(bottles(4).totalBottles, 3);
   });
 
   it('should return 0 with an investment of 0', () => {
-    assert.strictEqual(bottles(0), 0);
+    assert.strictEqual(bottles(0).totalBottles, 0);
   });
 
   it('should return 0 with an investment of less than 0', () => {
-    assert.strictEqual(bottles(-1), 0);
+    assert.strictEqual(bottles(-1).totalBottles, 0);
   });
 
   it('should return 0 when investment is not a number', () => {
-    assert.strictEqual(bottles('dog'), 0);
+    assert.strictEqual(bottles('dog').totalBottles, 0);
   });
 });
