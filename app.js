df4f38defd14305b9de44661edf98bf495226c5b
Matthew Thompson <supra__charged@hotmail.com>
2021-05-03 04:47:47 +0000

    Add printBottleReport in app.js to format program output

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const bottles = require('./bottles');
 const investment = process.argv[2];
 
+const printBottleReport = bottleData => {
+  const reportString = `
+  TOTAL BOTTLES: ${bottleData.totalBottles}
+  TOTAL PURCHASED: ${bottleData.purchased}
+  TOTAL EARNED:
+    BOTTLES: ${bottleData.recycling.bottles}
+    CAPS: ${bottleData.recycling.caps}
+ `;
+
+ console.log(reportString);
+};
+
 if (investment !== undefined) {
-  console.log(bottles(+investment));
+  const bottleData = bottles(+investment);
+  printBottleReport(bottleData);
 }
